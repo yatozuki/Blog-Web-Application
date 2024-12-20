6747f26a977cd9db2ffd763576f19b17ad84c13d
Zin Wai Htet <kozinwaihtet@gmail.com>
2024-12-21 03:51:10 +0700

    Finish Setup

---
+++ app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
