fcb98b18e96ddf1b5cb256b0b74e40574b593bf5
Zin Wai Htet <kozinwaihtet@gmail.com>
2024-12-22 01:23:24 +0700

    minor update

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+    
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 
 app.use(express.static('public'));
